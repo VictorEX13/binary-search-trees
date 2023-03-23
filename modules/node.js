7d05e1b05c66eae2c9997f4b17026fb909087727
Victor <victorshiira4869@gmail.com>
2023-03-23 10:24:20 -0300

    Create insert and remove methods on Tree class

--- modules/node.js
+++ modules/node.js
@@ -1,12 +1,8 @@
 class Node {
-  #data;
-  #left;
-  #right;
-
   constructor(data = null, left = null, right = null) {
-    this.#data = data;
-    this.#left = left;
-    this.#right = right;
+    this.data = data;
+    this.left = left;
+    this.right = right;
   }
 }
 
--- modules/tree.js
+++ modules/tree.js
@@ -1,22 +1,65 @@
 import Node from "./node";
 
 class Tree {
-  #root;
-
   constructor(arr) {
-    this.#root = this.#buildTree(arr);
+    this.root = this.buildTree(arr);
   }
 
-  #buildTree(arr) {
+  buildTree(arr) {
     if (arr.length === 0) return null;
 
     const mid = Math.floor(arr.length / 2);
     const rootNode = new Node(arr[mid]);
 
-    rootNode.left = this.#buildTree(arr.slice(0, mid));
-    rootNode.right = this.#buildTree(arr.slice(mid + 1));
+    rootNode.left = this.buildTree(arr.slice(0, mid));
+    rootNode.right = this.buildTree(arr.slice(mid + 1));
 
     return rootNode;
+  }
+
+  insert(treeRoot, value) {
+    if (!treeRoot) {
+      treeRoot = new Node(value);
+      return treeRoot;
+    }
+
+    if (value < treeRoot.data) {
+      treeRoot.left = this.insert(treeRoot.left, value);
+    } else if (value > treeRoot.data) {
+      treeRoot.right = this.insert(treeRoot.right, value);
+    }
+
+    return treeRoot;
+  }
+
+  delete(treeRoot, value) {
+    if (!treeRoot) {
+      return treeRoot;
+    }
+
+    if (value < treeRoot.data) {
+      treeRoot.left = this.delete(treeRoot.left, value);
+    } else if (value > treeRoot.data) {
+      treeRoot.right = this.delete(treeRoot.right, value);
+    } else {
+      if (!treeRoot.left) {
+        return treeRoot.right;
+      } else if (!treeRoot.right) {
+        return treeRoot.left;
+      }
+
+      let currentRoot = treeRoot.right;
+      let inorderSuccessor = currentRoot.data;
+      while (currentRoot.left) {
+        inorderSuccessor = currentRoot.left.data;
+        currentRoot = currentRoot.left;
+      }
+
+      treeRoot.data = inorderSuccessor;
+      treeRoot.right = this.delete(treeRoot.right, inorderSuccessor);
+    }
+
+    return treeRoot;
   }
 }
 
